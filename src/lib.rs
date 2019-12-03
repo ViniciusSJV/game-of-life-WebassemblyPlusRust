bf171336d8a3d7218943aaf0b6eb5e8c8ad14088
João Vinicius <vinicius.souza@agileprocess.com.br>
2019-12-03 16:09:53 -0300

    'add_test'

--- src/lib.rs
+++ src/lib.rs
@@ -217,9 +217,19 @@ impl Universe {
 	pub fn width(&self) -> u32 {
         self.width
     }
+	
+	pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
+    }
 
     pub fn height(&self) -> u32 {
         self.height
+    }
+	
+	 pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
     }
 
     pub fn cells(&self) -> *const Cell {
@@ -291,6 +301,23 @@ impl Universe {
 		index = self.get_index(row - 1, col - 1);
 		self.cells[index] = Cell::Alive; 
 	}
+}
+
+impl Universe {
+    /// Get the dead and alive values of the entire universe.
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    /// Set cells to be alive in a universe by passing the row and column
+    /// of each cell as an array.
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells[idx] = Cell::Alive;
+        }
+    }
+
 }
 
 impl Cell {
